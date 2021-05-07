9ac58c68116ea5b4891c4918c6a38bf1f1fc246a
mjeel <yasminsheik05@gmail.com>
2021-05-07 12:33:41 +0530

    resource

--- 04-resources/sample.tf
+++ 04-resources/sample.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "sample" {
   ami                       = "ami-079a3f3cf00741286"
   instance_type             = "t2.micro"
+  vpc_security_group_ids    = [aws_security_group.allow_ssh.id]
 }
 
 resource "aws_security_group" "allow_ssh" {
