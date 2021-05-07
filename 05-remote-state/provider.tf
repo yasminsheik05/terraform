e310a3134bab528e43ab61e3e4fb3442b513f337
mjeel <yasminsheik05@gmail.com>
2021-05-07 14:58:05 +0530

    remote

--- 05-remote-state/provider.tf
+++ 05-remote-state/provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket     = "yasmin-terraform-state-files "
+    key        = "sample/terraform.tfstate"
+    regoin     = "us-east-1"
+  }
+}
