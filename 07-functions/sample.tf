2ae708b4b0f32835601dc8b1c0da9bd16450b0c0
mjeel <yasminsheik05@gmail.com>
2021-05-07 17:15:09 +0530

    functions 1

--- 07-functions/sample.tf
+++ 07-functions/sample.tf
@@ -9,3 +9,16 @@ resource "aws_instance" "sample" {
 variable "name" {
   default = "sample"
 }
+
+variable "image_id" {
+  type  = string
+  description = "the id of the machine image (AMI) to use for the server"
+  validation {
+    condition = length(var.image_id) > 4 && substr(var.image_id, 0, 4 ) == "ami-"
+    error_message = "the image_id value must be avoid AMI id, satrting with \"ami-\"."
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
